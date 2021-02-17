b676cba6d5af291b6fce619c261fbf95b38065d3
vagrant <vagrant@homestead>
2021-02-17 16:34:35 +0000

    changes in dashboard

--- resources/views/apps/Core/Admin/index.blade.php
+++ resources/views/apps/Core/Admin/index.blade.php
@@ -16,7 +16,7 @@
 
 
 
-			@if(Auth::user()->role=='superadmin')
+				@if(Auth::user()->role=='superadmin')
 				<div class='col-6 col-md-4'>
 					<!--begin::Tiles Widget 11-->
 					<div class="card card-custom gutter-b" style="max-height: 150px;background:#ec65c8;">
@@ -107,23 +107,46 @@
 					<!--end::Tiles Widget 11-->
 				</div>
 
+				
 				<div class='col-6 col-md-4'>
 					<!--begin::Tiles Widget 11-->
-					<div class="card card-custom   gutter-b" style="max-height: 150px;background:#ff5500;">
+					<div class="card card-custom  gutter-b" style="max-height: 150px;background:#4b9c77">
 						<div class="card-body">
 							<span class="svg-icon svg-icon-3x svg-icon-white ml-n2">
-								<!--begin::Svg Icon | path:/metronic/theme/html/demo1/dist/assets/media/svg/icons/Layout/Layout-4-blocks.svg-->
-								<span class="svg-icon svg-icon-light svg-icon-2x"><!--begin::Svg Icon | path:/var/www/preview.keenthemes.com/metronic/releases/2021-02-01-052524/theme/html/demo1/dist/../src/media/svg/icons/Design/Brush.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+								<span class="svg-icon svg-icon-white svg-icon-2x"><!--begin::Svg Icon | path:/var/www/preview.keenthemes.com/metronic/releases/2021-02-01-052524/theme/html/demo1/dist/../src/media/svg/icons/Shopping/Wallet.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
 									<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
 										<rect x="0" y="0" width="24" height="24"/>
-										<path d="M13.5,6 C13.33743,8.28571429 12.7799545,9.78571429 11.8275735,10.5 C11.8275735,10.5 12.5,4 10.5734853,2 C10.5734853,2 10.5734853,5.92857143 8.78777106,9.14285714 C7.95071887,10.6495511 7.00205677,12.1418252 7.00205677,14.1428571 C7.00205677,17 10.4697177,18 12.0049375,18 C13.8025422,18 17,17 17,13.5 C17,12.0608202 15.8333333,9.56082016 13.5,6 Z" fill="#000000"/>
-										<path d="M9.72075922,20 L14.2792408,20 C14.7096712,20 15.09181,20.2754301 15.2279241,20.6837722 L16,23 L8,23 L8.77207592,20.6837722 C8.90818997,20.2754301 9.29032881,20 9.72075922,20 Z" fill="#000000" opacity="0.3"/>
+										<circle fill="#000000" opacity="0.3" cx="20.5" cy="12.5" r="1.5"/>
+										<rect fill="#000000" opacity="0.3" transform="translate(12.000000, 6.500000) rotate(-15.000000) translate(-12.000000, -6.500000) " x="3" y="3" width="18" height="7" rx="1"/>
+										<path d="M22,9.33681558 C21.5453723,9.12084552 21.0367986,9 20.5,9 C18.5670034,9 17,10.5670034 17,12.5 C17,14.4329966 18.5670034,16 20.5,16 C21.0367986,16 21.5453723,15.8791545 22,15.6631844 L22,18 C22,19.1045695 21.1045695,20 20,20 L4,20 C2.8954305,20 2,19.1045695 2,18 L2,6 C2,4.8954305 2.8954305,4 4,4 L20,4 C21.1045695,4 22,4.8954305 22,6 L22,9.33681558 Z" fill="#000000"/>
 									</g>
-								</svg><!--end::Svg Icon--></span>
-								<!--end::Svg Icon-->
+								</svg><!--end::Svg Icon--></span> 
 							</span>
 							<div>
-								<a href="{{ url('admin/theme')}}" class="text-inverse-success font-weight-bolder font-size-h4 mt-3">Themes</a>
+								<a href="{{ url('admin/billing')}}" class="text-inverse-success font-weight-bolder font-size-h4 mt-3">Billing</a>
+							</div>
+						</div>
+					</div>
+					<!--end::Tiles Widget 11-->
+				</div>
+
+				<div class='col-6 col-md-4'>
+					<!--begin::Tiles Widget 11-->
+					<div class="card card-custom  gutter-b" style="max-height: 150px;background:#3f51b5">
+						<div class="card-body">
+							<span class="svg-icon svg-icon-3x svg-icon-white ml-n2">
+								<span class="svg-icon svg-icon-white svg-icon-2x"><!--begin::Svg Icon | path:/var/www/preview.keenthemes.com/metronic/releases/2021-02-01-052524/theme/html/demo1/dist/../src/media/svg/icons/Media/Equalizer.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+									<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+										<rect x="0" y="0" width="24" height="24"/>
+										<rect fill="#000000" opacity="0.3" x="13" y="4" width="3" height="16" rx="1.5"/>
+										<rect fill="#000000" x="8" y="9" width="3" height="11" rx="1.5"/>
+										<rect fill="#000000" x="18" y="11" width="3" height="9" rx="1.5"/>
+										<rect fill="#000000" x="3" y="13" width="3" height="7" rx="1.5"/>
+									</g>
+								</svg><!--end::Svg Icon--></span> 
+							</span>
+							<div>
+								<a href="{{ url('admin/statistics')}}" class="text-inverse-success font-weight-bolder font-size-h4 mt-3">Statistics</a>
 							</div>
 						</div>
 					</div>
@@ -167,6 +190,26 @@
 				</svg><!--end::Svg Icon--></span> Applications</h4>
 				<div class="row">
 
+					<div class='col-6 col-md-4'>
+						<!--begin::Tiles Widget 11-->
+						<div class="card card-custom   gutter-b" style="max-height: 150px;background:#ff5500;">
+							<div class="card-body">
+								<span class="svg-icon svg-icon-3x svg-icon-white ml-n2">
+									<span class="svg-icon svg-icon-white svg-icon-2x"><!--begin::Svg Icon | path:/var/www/preview.keenthemes.com/metronic/releases/2021-02-01-052524/theme/html/demo1/dist/../src/media/svg/icons/Layout/Layout-top-panel-3.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+										<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+											<rect x="0" y="0" width="24" height="24"/>
+											<path d="M3,4 L20,4 C20.5522847,4 21,4.44771525 21,5 L21,7 C21,7.55228475 20.5522847,8 20,8 L3,8 C2.44771525,8 2,7.55228475 2,7 L2,5 C2,4.44771525 2.44771525,4 3,4 Z M3,10 L13,10 C13.5522847,10 14,10.4477153 14,11 L14,19 C14,19.5522847 13.5522847,20 13,20 L3,20 C2.44771525,20 2,19.5522847 2,19 L2,11 C2,10.4477153 2.44771525,10 3,10 Z" fill="#000000"/>
+											<rect fill="#000000" opacity="0.3" x="16" y="10" width="5" height="10" rx="1"/>
+										</g>
+									</svg><!--end::Svg Icon--></span>
+								</span>
+								<div>
+									<a href="{{ url('admin/theme')}}" class="text-inverse-success font-weight-bolder font-size-h4 mt-3">Theme</a>
+								</div>
+							</div>
+						</div>
+						<!--end::Tiles Widget 11-->
+					</div>
 					<div class='col-6 col-md-4'>
 						<!--begin::Tiles Widget 11-->
 						<div class="card card-custom  gutter-b" style="max-height: 150px;background:#4bc350;">
@@ -234,6 +277,96 @@
 						</div>
 						<!--end::Tiles Widget 11-->
 					</div>
+
+					<div class='col-6 col-md-4'>
+						<!--begin::Tiles Widget 11-->
+						<div class="card card-custom gutter-b" style="max-height: 150px;background:#e91e63;">
+							<div class="card-body">
+								<span class="svg-icon svg-icon-3x svg-icon-white ml-n2">
+									<span class="svg-icon svg-icon-white svg-icon-2x"><!--begin::Svg Icon | path:/var/www/preview.keenthemes.com/metronic/releases/2021-02-01-052524/theme/html/demo1/dist/../src/media/svg/icons/Communication/Mail-opened.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+										<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+											<rect x="0" y="0" width="24" height="24"/>
+											<path d="M6,2 L18,2 C18.5522847,2 19,2.44771525 19,3 L19,12 C19,12.5522847 18.5522847,13 18,13 L6,13 C5.44771525,13 5,12.5522847 5,12 L5,3 C5,2.44771525 5.44771525,2 6,2 Z M7.5,5 C7.22385763,5 7,5.22385763 7,5.5 C7,5.77614237 7.22385763,6 7.5,6 L13.5,6 C13.7761424,6 14,5.77614237 14,5.5 C14,5.22385763 13.7761424,5 13.5,5 L7.5,5 Z M7.5,7 C7.22385763,7 7,7.22385763 7,7.5 C7,7.77614237 7.22385763,8 7.5,8 L10.5,8 C10.7761424,8 11,7.77614237 11,7.5 C11,7.22385763 10.7761424,7 10.5,7 L7.5,7 Z" fill="#000000" opacity="0.3"/>
+											<path d="M3.79274528,6.57253826 L12,12.5 L20.2072547,6.57253826 C20.4311176,6.4108595 20.7436609,6.46126971 20.9053396,6.68513259 C20.9668779,6.77033951 21,6.87277228 21,6.97787787 L21,17 C21,18.1045695 20.1045695,19 19,19 L5,19 C3.8954305,19 3,18.1045695 3,17 L3,6.97787787 C3,6.70173549 3.22385763,6.47787787 3.5,6.47787787 C3.60510559,6.47787787 3.70753836,6.51099993 3.79274528,6.57253826 Z" fill="#000000"/>
+										</g>
+									</svg><!--end::Svg Icon--></span>
+								</span>
+								<div>
+									<a href="{{ url('admin/mailer')}}" class="text-inverse-success font-weight-bolder font-size-h4 mt-3">Mailer</a>
+								</div>
+							</div>
+						</div>
+						<!--end::Tiles Widget 11-->
+					</div>
+
+					<div class='col-6 col-md-4'>
+						<!--begin::Tiles Widget 11-->
+						<div class="card card-custom gutter-b" style="max-height: 150px;background:#607d8b;">
+							<div class="card-body">
+								<span class="svg-icon svg-icon-3x svg-icon-white ml-n2">
+									<span class="svg-icon svg-icon-white svg-icon-2x"><!--begin::Svg Icon | path:/var/www/preview.keenthemes.com/metronic/releases/2021-02-01-052524/theme/html/demo1/dist/../src/media/svg/icons/Design/Pixels.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+										<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+											<rect x="0" y="0" width="24" height="24"/>
+											<rect fill="#000000" x="4" y="16" width="4" height="4" rx="1"/>
+											<rect fill="#000000" x="4" y="10" width="4" height="4" rx="1"/>
+											<rect fill="#000000" x="10" y="16" width="4" height="4" rx="1"/>
+											<rect fill="#000000" opacity="0.3" x="10" y="10" width="4" height="4" rx="1"/>
+											<rect fill="#000000" x="4" y="4" width="4" height="4" rx="1"/>
+											<rect fill="#000000" x="16" y="16" width="4" height="4" rx="1"/>
+										</g>
+									</svg><!--end::Svg Icon--></span>
+								</span>
+								<div>
+									<a href="{{ url('admin/crm')}}" class="text-inverse-success font-weight-bolder font-size-h4 mt-3">CRM</a>
+								</div>
+							</div>
+						</div>
+						<!--end::Tiles Widget 11-->
+					</div>
+
+					<div class='col-6 col-md-4'>
+						<!--begin::Tiles Widget 11-->
+						<div class="card card-custom gutter-b" style="max-height: 150px;background:#1977a5;">
+							<div class="card-body">
+								<span class="svg-icon svg-icon-3x svg-icon-white ml-n2">
+									<span class="svg-icon svg-icon-white svg-icon-2x"><!--begin::Svg Icon | path:/var/www/preview.keenthemes.com/metronic/releases/2021-02-01-052524/theme/html/demo1/dist/../src/media/svg/icons/General/Like.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+										<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+											<rect x="0" y="0" width="24" height="24"/>
+											<path d="M9,10 L9,19 L10.1525987,19.3841996 C11.3761964,19.7920655 12.6575468,20 13.9473319,20 L17.5405883,20 C18.9706314,20 20.2018758,18.990621 20.4823303,17.5883484 L21.231529,13.8423552 C21.5564648,12.217676 20.5028146,10.6372006 18.8781353,10.3122648 C18.6189212,10.260422 18.353992,10.2430672 18.0902299,10.2606513 L14.5,10.5 L14.8641964,6.49383981 C14.9326895,5.74041495 14.3774427,5.07411874 13.6240179,5.00562558 C13.5827848,5.00187712 13.5414031,5 13.5,5 L13.5,5 C12.5694044,5 11.7070439,5.48826024 11.2282564,6.28623939 L9,10 Z" fill="#000000"/>
+											<rect fill="#000000" opacity="0.3" x="2" y="9" width="5" height="11" rx="1"/>
+										</g>
+									</svg><!--end::Svg Icon--></span>
+								</span>
+								<div>
+									<a href="{{ url('admin/social')}}" class="text-inverse-success font-weight-bolder font-size-h4 mt-3">Social</a>
+								</div>
+							</div>
+						</div>
+						<!--end::Tiles Widget 11-->
+					</div>
+
+					<div class='col-6 col-md-4'>
+						<!--begin::Tiles Widget 11-->
+						<div class="card card-custom  gutter-b" style="max-height: 150px;background:#11cee6;">
+							<div class="card-body">
+								<span class="svg-icon svg-icon-3x svg-icon-white ml-n2">
+									<span class="svg-icon svg-icon-white svg-icon-2x"><!--begin::Svg Icon | path:/var/www/preview.keenthemes.com/metronic/releases/2021-02-01-052524/theme/html/demo1/dist/../src/media/svg/icons/Shopping/Cart2.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+										<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+											<rect x="0" y="0" width="24" height="24"/>
+											<path d="M12,4.56204994 L7.76822128,9.6401844 C7.4146572,10.0644613 6.7840925,10.1217854 6.3598156,9.76822128 C5.9355387,9.4146572 5.87821464,8.7840925 6.23177872,8.3598156 L11.2317787,2.3598156 C11.6315738,1.88006147 12.3684262,1.88006147 12.7682213,2.3598156 L17.7682213,8.3598156 C18.1217854,8.7840925 18.0644613,9.4146572 17.6401844,9.76822128 C17.2159075,10.1217854 16.5853428,10.0644613 16.2317787,9.6401844 L12,4.56204994 Z" fill="#000000" fill-rule="nonzero" opacity="0.3"/>
+											<path d="M3.28077641,9 L20.7192236,9 C21.2715083,9 21.7192236,9.44771525 21.7192236,10 C21.7192236,10.0817618 21.7091962,10.163215 21.6893661,10.2425356 L19.5680983,18.7276069 C19.234223,20.0631079 18.0342737,21 16.6576708,21 L7.34232922,21 C5.96572629,21 4.76577697,20.0631079 4.43190172,18.7276069 L2.31063391,10.2425356 C2.17668518,9.70674072 2.50244587,9.16380623 3.03824078,9.0298575 C3.11756139,9.01002735 3.1990146,9 3.28077641,9 Z M12,12 C11.4477153,12 11,12.4477153 11,13 L11,17 C11,17.5522847 11.4477153,18 12,18 C12.5522847,18 13,17.5522847 13,17 L13,13 C13,12.4477153 12.5522847,12 12,12 Z M6.96472382,12.1362967 C6.43125772,12.2792385 6.11467523,12.8275755 6.25761704,13.3610416 L7.29289322,17.2247449 C7.43583503,17.758211 7.98417199,18.0747935 8.51763809,17.9318517 C9.05110419,17.7889098 9.36768668,17.2405729 9.22474487,16.7071068 L8.18946869,12.8434035 C8.04652688,12.3099374 7.49818992,11.9933549 6.96472382,12.1362967 Z M17.0352762,12.1362967 C16.5018101,11.9933549 15.9534731,12.3099374 15.8105313,12.8434035 L14.7752551,16.7071068 C14.6323133,17.2405729 14.9488958,17.7889098 15.4823619,17.9318517 C16.015828,18.0747935 16.564165,17.758211 16.7071068,17.2247449 L17.742383,13.3610416 C17.8853248,12.8275755 17.5687423,12.2792385 17.0352762,12.1362967 Z" fill="#000000"/>
+										</g>
+									</svg><!--end::Svg Icon--></span>
+								</span>
+								<div>
+									<a href="{{ url('admin/shop')}}" class="text-inverse-success font-weight-bolder font-size-h4 mt-3">Shop</a>
+								</div>
+							</div>
+						</div>
+						<!--end::Tiles Widget 11-->
+					</div>
+
+
 
 
 				</div>
