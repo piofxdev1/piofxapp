d61b235bae2b3bde8da4a3b79f22d3de98134d07
Krishna Teja <founder@packetprep.com>
2021-03-12 15:20:18 +0530

    updated contact form

--- resources/views/apps/Core/Contact/index.blade.php
+++ resources/views/apps/Core/Contact/index.blade.php
@@ -210,7 +210,7 @@
         </div>
         @endif
         <nav aria-label="Page navigation  " class="card-nav @if($objs->total() > config('global.no_of_records'))mt-3 @endif">
-        {{$objs->appends(request()->input('status', ''))->links()  }}
+        {{$objs->appends(request()->query('status'))->links()  }}
       </nav>
   </x-snippets.cards.basic>
   <!--end::basic card-->
