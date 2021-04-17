262eaa04a33c14f9001a0b56fec16b0c2dae9298
Krishna Teja <founder@packetprep.com>
2021-04-17 21:52:26 +0530

    some changes to test.js

--- public/js/test.js
+++ public/js/test.js
@@ -1,29 +1,32 @@
 $(function(){
 
-    if($(".test_container").length){
-        $url = $(".test_container").data('url');
-        $.get($url,function(data){
-                
-            $(".test_container").html(data);
-                console.log('test data pulled');
+    // if($(".test_container").length){
+    //     $url = $(".test_container").data('url');
+    //     $.get($url,function(data){
+    //         $(".test_container").html(data);
+    //             console.log('test data pulled');
+    //         });    
+    // }
 
-               
-            });    
-    }
+    var prefix_url = 'https://prep.firstacademy.in/';
 
     if($("form").length){
-        $url = $("form").attr('action');
-        $url1 = $(".test_container").data('url2');
+        
         $(document).on('click', '.ajaxtestsubmit', function(e) {
+            var slug = $(this).data('test');
+            var container= $("."+slug);
+            var url = submitTestUrl(slug);
             e.preventDefault();
 
-            var formValues= $('.test').serialize();
-            console.log(formValues);
-            $.get($url1, formValues, function(data){
-                $(".test_container").html("<div class='bg-white w-100 p-3 h4'>Your score is "+data+"</div><button class='btn bnt-primary showanswers'>Show Answers</button>");
+            var formValues= $('.form_'+slug).serialize();
+         
+            $.get(url, formValues, function(data){
+                var d = JSON.parse(data);
+                console.log(d);
+                container.html("<div class='bg-white w-100 p-3 h4'><p>Your score is "+d.score+"/"+d.total+"</p><button class='btn btn-soft-primary showanswers' data-container='"+slug+"'>Show Answers</button><button class='btn btn-soft-success ml-2 trytest' data-container='"+slug+"'>Retry Test</button></div>");
                 $('html, body').animate({
-        scrollTop: $(".test_container").offset().top - 200
-    }, 500);
+                    scrollTop: container.offset().top - 200
+                }, 500);
 
             });
         });
@@ -33,14 +36,38 @@ $(function(){
     }
 
     $(document).on('click', '.showanswers', function(e) {
-        $url = $(".test_container").data('url')+'?answers=1';
-        $.get($url,function(data){
-                
-            $(".test_container").html(data);
+           e.preventDefault();
+        var slug = $(this).data('container');
+        var container = $("."+slug);
+        var url = testUrl(slug,true);
+        $.get(url,function(data){
+            container.html(data).append("<p class='mt-3'><button class='btn btn-soft-success ml-2 trytest' data-container='"+slug+"'>Retry Test</button></p>");
                 console.log('test data pulled');
-
-               
             });    
     });
+
+    $(document).on('click', '.trytest', function(e) {
+           e.preventDefault();
+        var slug = $(this).data('container');
+        var container = $("."+slug);
+        var url = testUrl(slug);
+        $.get(url,function(data){
+            container.html(data);
+            $('html, body').animate({
+                    scrollTop: container.offset().top - 200
+            }, 500);
+
+        });    
+    });
+
+    function submitTestUrl(slug){
+        return prefix_url+'apitestget/'+slug;
+    }
 	
+    function testUrl(slug,answers=false){
+        if(answers)
+            return prefix_url+'apitest/'+slug+'?answers=1';
+        else
+            return prefix_url+'apitest/'+slug;
+    }
 });
