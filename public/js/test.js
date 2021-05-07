7da406cb781b665a24a71b2c85b62f84165253f2
Krishna Teja <founder@packetprep.com>
2021-05-07 15:15:21 +0530

    some changes to test embed

--- public/js/test.js
+++ public/js/test.js
@@ -1,38 +1,49 @@
 $(function(){
 
-    // if($(".test_container").length){
-    //     $url = $(".test_container").data('url');
-    //     $.get($url,function(data){
-    //         $(".test_container").html(data);
-    //             console.log('test data pulled');
-    //         });    
-    // }
-
     var prefix_url = 'https://prep.firstacademy.in/';
 
-        
-        $(document).on('click', '.ajaxtestsubmit', function(e) {
-            var slug = $(this).data('test');
-            var container= $("."+slug);
-            var url = submitTestUrl(slug);
-            e.preventDefault();
+    if($(".test-container").length){
 
-            var formValues= $('.form_'+slug).serialize();
-            console.log(formValues);
-         
-            $.get(url, formValues, function(data){
-                var d = JSON.parse(data);
-                console.log(d);
-                container.html("<div class='bg-white w-100 p-3 h4'><p>Your score is "+d.score+"/"+d.total+"</p><button class='btn btn-soft-primary showanswers' data-container='"+slug+"'>Show Answers</button><button class='btn btn-soft-success ml-2 trytest' data-container='"+slug+"'>Retry Test</button></div>");
+        $( ".test-container" ).each(function() {
+            var slug = $(this).data('container');
+            var container = $(this);
+            var url = testUrl(slug);
+            $.get(url,function(data){
+                container.html(data);
                 $('html, body').animate({
-                    scrollTop: container.offset().top - 200
+                        scrollTop: container.offset().top - 200
                 }, 500);
 
-            });
+            });    
         });
+        
+    }
+
+   
+
+        
+    $(document).on('click', '.ajaxtestsubmit', function(e) {
+        var slug = $(this).data('test');
+        var container= $("."+slug);
+        var url = submitTestUrl(slug);
+        e.preventDefault();
+
+        var formValues= $('.form_'+slug).serialize();
+        console.log(formValues);
+        
+        $.get(url, formValues, function(data){
+            var d = JSON.parse(data);
+            console.log(d);
+            container.html("<div class='bg-white w-100 p-3 h4'><p>Your score is "+d.score+"/"+d.total+"</p><button class='btn btn-soft-primary showanswers' data-container='"+slug+"'>Show Answers</button><button class='btn btn-soft-success ml-2 trytest' data-container='"+slug+"'>Retry Test</button></div>");
+            $('html, body').animate({
+                scrollTop: container.offset().top - 200
+            }, 500);
+
+        });
+    });
 
       
-        
+    
     
 
     $(document).on('click', '.showanswers', function(e) {
